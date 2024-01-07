bde031a5f0563c2c1f010da64ea69ae587db78db
Igor <33967454+utilizable@users.noreply.github.com>
2024-01-07 02:39:57 +0100

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,10 @@
+# main.tf
+
+module "module" {
+  // source module
+  source = "git::https://github.com/localstack-samples/localstack-terraform-samples.git//websocket-deploy"
+
+  // pass provider variables to module 
+
+  // pass variables to module
+}
