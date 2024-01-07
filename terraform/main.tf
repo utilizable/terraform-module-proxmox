517770e9c9fba2f14dac45dbfc8b22d9e852c2a1
utilizable <igor.sadza@gmail.com>
2024-01-07 01:55:00 +0000

    append basis for terraform module; small docker-compose corrections;

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,13 @@
 # main.tf
 
-module "module" {
+module "main" {
   // source module
-  source = "git::https://github.com/localstack-samples/localstack-terraform-samples.git//websocket-deploy"
+  source = "./modules/base" 
 
-  // pass provider variables to module 
+  // pass provider variables to module  - proxmox
+  provider_proxmox_username = var.provider_proxmox_username
+  provider_proxmox_password = var.provider_proxmox_password
+  provider_proxmox_endpoint = var.provider_proxmox_endpoint
 
   // pass variables to module
 }
